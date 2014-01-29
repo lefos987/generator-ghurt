d5a63140d61e24219da647138fbf9ad2bb16c9e8
Max Duluc <max.duluc@capgemini.com>
2014-01-29 10:14:18 +0000

    Update hapi-util tests generated for a cleaner version

--- hapi-util/templates/_test.spec.js
+++ hapi-util/templates/_test.spec.js
@@ -2,51 +2,49 @@
 
 var sinon = require('sinon');
 var rewire = require('rewire');
-var <%= tpl.routeName %> = rewire('../../../../src/api/<%= tpl.routePath %>');
+var <%= tpl.routeName %> = require('../../../../src/api/<%= tpl.routePath %>');
 var logger = require('../../../../src/api/util/logger');
+var Hapi = require('Hapi');
 var Q = require('q');
 
 describe('<%= tpl.routeName %> controller', function () {
 
 	describe('handler', function () {
 
-		var logStub, responseStub, codeStub, requestMock, responseMock;
+		var logStub, server;
+		server = new Hapi.Server();
+		server.route({
+			method: 'GET',
+			path: '/',
+			config: {
+				handler: <%= tpl.routeName %>.<%= tpl.routeName %>Handler
+			}
+		});
 
 		beforeEach(function () {
-			requestMock = {
-				params: {}
-			};
 			logStub = sinon.stub(logger, 'log');
-
-			codeStub = function () {};
-			responseMock = function () {
-				return {
-					code: codeStub
-				};
-			};
-			responseStub = sinon.spy(responseMock);
 		});
 
 		afterEach(function () {
 			logStub.restore();
 		});
 
-		it('should call the logger', function () {
-			<%= tpl.routeName %>.<%= tpl.routeName %>Handler(requestMock, responseMock);
-			expect(logStub.called).toBe(true);
-			expect(logStub.args[0][0]).toEqual('info');
-			expect(logStub.args[0][2].tags).toEqual(['app_info']);
-			expect(logStub.args[0][2].method).toEqual('<%= tpl.routeName %>');
+		it('should call the logger', function (done) {
+			server.inject('/', function (res) {
+				expect(logStub.called).toBe(true);
+				expect(logStub.args[0][0]).toEqual('info');
+				expect(logStub.args[0][2].tags).toEqual(['app_info']);
+				expect(logStub.args[0][2].method).toEqual('<%= tpl.routeName %>');
+				done();
+			});
 		});
 
 		it('should reply a success', function (done) {
-			codeStub = function (code) {
-				expect(responseStub.called).toBe(true);
-				expect(responseStub.args[0][0]).toEqual({success: true});
-				expect(code).toEqual(200);
+			server.inject('/', function (res) {
+				expect(res.result).toEqual({success: true});
+				expect(res.statusCode).toEqual(200);
 				done();
-			};
-			<%= tpl.routeName %>.<%= tpl.routeName %>Handler(requestMock, responseStub);
+			});
 		});
 
 	});
